43565270f4fb898589eafaf44370f8b8bb764309
Rafael Persiguelo <rafinhapersiguelo@gmail.com>
2026-03-10 23:57:23 -0300

    Add files via upload

---
+++ escola-api/src/test/java/com/example/escola_api/EscolaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.escola_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
